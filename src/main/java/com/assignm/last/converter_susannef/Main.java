9bb3c6b270d66622d5c361a2f1c12082dd7b62e4
Susanne Fridh <fridh.susanne@gmail.com>
2017-12-11 13:59:28 +0100

    I have updated javadoc on Main class.

--- src/main/java/com/assignm/last/converter_susannef/Main.java
+++ src/main/java/com/assignm/last/converter_susannef/Main.java
@@ -1,7 +1,18 @@
+/**
+ * @author susanne fridh
+ * @version 1.0
+ * 
+ * This is the Main class.
+ */
+
 package com.assignm.last.converter_susannef;
 
 public class Main {
 
+	/**
+	 * Main method with only declaration of mainframe ConverterGUI.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ConverterGUI cgui = new ConverterGUI();
 
